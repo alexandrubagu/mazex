ce4ce6751f0d73e331e908dfa750117519e05e32
Alexandru Bagu <alexandrubagu87@gmail.com>
2024-02-14 10:11:20 +0200

    project init

---
+++ mix.exs
@@ -0,0 +1,27 @@
+defmodule Mazex.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :mazex,
+      version: "0.1.0",
+      elixir: "~> 1.15",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:egd, github: "erlang/egd", tag: "0.10.0"}
+    ]
+  end
+end
