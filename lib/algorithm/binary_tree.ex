a935f6ddcf065d4178ac08b20037c0bba9e0c672
Alexandru Bagu <alexandrubagu87@gmail.com>
2024-03-11 22:32:12 +0200

    small refactor for binary tree algorithm

--- lib/algorithm/binary_tree.ex
+++ lib/algorithm/binary_tree.ex
@@ -8,43 +8,20 @@ defmodule Algorithm.BinaryTree do
 
   def run(grid) do
     for x <- 0..(grid.rows - 1), y <- 0..(grid.columns - 1), reduce: grid do
-      acc -> carve_passages(_cell_position = {x, y}, acc)
+      acc -> carve_passage(_cell_position = {x, y}, acc)
     end
   end
 
-  defp carve_passages(cell_position, grid) do
-    cell = Grid.get(grid, cell_position)
+  defp carve_passage(cell_position, grid) do
+    possible_directions =
+      Enum.filter([:east, :south], &Grid.get_neighbor(&1, cell_position, grid))
 
-    case random_neighbor(cell, grid) do
+    case random_direction(possible_directions) do
       nil -> grid
-      {direction, neighbor} -> update_cell_walls(cell, neighbor, direction, grid)
+      direction -> Grid.carve_passage(direction, cell_position, grid)
     end
   end
 
-  defp random_neighbor(cell, grid) do
-    possible_neighbors = [
-      east: {cell.row, cell.column + 1},
-      south: {cell.row + 1, cell.column}
-    ]
-
-    neighbors =
-      for {direction, possible_neighbor_position} <- possible_neighbors,
-          neighbor = Grid.get(grid, possible_neighbor_position),
-          reduce: [] do
-        acc -> [{direction, neighbor} | acc]
-      end
-
-    if not Enum.empty?(neighbors), do: Enum.random(neighbors)
-  end
-
-  defp update_cell_walls(cell, neighbor, direction, grid) do
-    updated_cell = Grid.Cell.remove_wall(cell, direction)
-
-    neighbor_opposite_direction = Grid.Cell.opposite_direction(direction)
-    updated_neighbor = Grid.Cell.remove_wall(neighbor, neighbor_opposite_direction)
-
-    grid
-    |> Grid.put(updated_cell)
-    |> Grid.put(updated_neighbor)
-  end
+  defp random_direction([]), do: nil
+  defp random_direction(directions), do: Enum.random(directions)
 end
--- lib/grid.ex
+++ lib/grid.ex
@@ -3,6 +3,17 @@ defmodule Grid do
   Defines a grid and its cells.
   """
 
+  @type t :: %__MODULE__{
+          rows: pos_integer(),
+          columns: pos_integer(),
+          lookup: %{cell_position => Cell.t()}
+        }
+
+  @typep direction :: :east | :south | :west | :north
+  @typep cell_position :: {row :: pos_integer(), column :: pos_integer()}
+
+  defstruct [:rows, :columns, lookup: %{}]
+
   defmodule Cell do
     @moduledoc """
     Defines a cell in a grid.
@@ -18,89 +29,91 @@ defmodule Grid do
             }
           }
 
-    @enforce_keys [:row, :column]
+    @typep direction :: :east | :south | :west | :north
 
-    defstruct [:row, :column, walls: %{north: true, east: true, south: true, west: true}]
+    defstruct [:row, :column, walls: %{}]
 
+    @doc """
+    Creates a new cell.
+    """
     @spec new(pos_integer(), pos_integer()) :: t()
-
-    def new(row, column), do: %__MODULE__{row: row, column: column}
+    def new(row, column) do
+      %__MODULE__{
+        row: row,
+        column: column,
+        walls: %{north: true, east: true, south: true, west: true}
+      }
+    end
 
     @doc """
     Helper function to remove a wall from a cell.
     """
-    @spec remove_wall(t(), :north | :east | :south | :west) :: t()
+    @spec remove_wall(t(), direction()) :: t()
     def remove_wall(cell, :north), do: %{cell | walls: %{cell.walls | north: false}}
     def remove_wall(cell, :east), do: %{cell | walls: %{cell.walls | east: false}}
     def remove_wall(cell, :south), do: %{cell | walls: %{cell.walls | south: false}}
     def remove_wall(cell, :west), do: %{cell | walls: %{cell.walls | west: false}}
-
-    @doc """
-    Helper function to return the opposite direction of a given direction.
-    """
-    @spec opposite_direction(:east | :south | :west | :north) :: :east | :south | :west | :north
-    def opposite_direction(:east), do: :west
-    def opposite_direction(:south), do: :north
-    def opposite_direction(:west), do: :east
-    def opposite_direction(:north), do: :south
   end
 
-  @typep cell_position :: {row :: pos_integer(), column :: pos_integer()}
-
-  @type t :: %__MODULE__{
-          rows: pos_integer(),
-          columns: pos_integer(),
-          lookup: %{cell_position => Cell.t()}
-        }
-
-  defstruct [:rows, :columns, lookup: %{}]
-
-  @doc ~S"""
+  @doc """
   Creates a new grid.
-
-  ## Example:
-
-      iex> Grid.new(2, 2)
-      %Grid{rows: 2, columns: 2, lookup: %{
-        {0, 0} => %Cell{row: 0, column: 0, walls: %{east: true, north: true, south: true, west: true}},
-        {0, 1} => %Cell{row: 0, column: 1, walls: %{east: true, north: true, south: true, west: true}},
-        {1, 0} => %Cell{row: 1, column: 0, walls: %{east: true, north: true, south: true, west: true}},
-        {1, 1} => %Cell{row: 1, column: 1, walls: %{east: true, north: true, south: true, west: true}}
-      }}
   """
   @spec new(pos_integer(), pos_integer()) :: t()
   def new(rows, columns) do
     grid = %__MODULE__{rows: rows, columns: columns}
 
-    for row <- 0..(rows - 1),
-        column <- 0..(columns - 1),
-        reduce: grid do
+    for row <- 0..(rows - 1), column <- 0..(columns - 1), reduce: grid do
       acc -> put(acc, Cell.new(row, column))
     end
   end
 
   @doc """
   Adds a cell to the grid.
-
-  ## Example:
-    iex> grid = Grid.new(2, 2)
-    iex> cell = Grid.Cell.new(0, 0)
-    iex> Grid.put(grid, cell)
-    %Grid{width: 2, height: 2, lookup: %{
-      {0, 0} => %Maze.Cell{row: 0, column: 0, walls: %{east: true, north: true, south: true, west: true}}
-    }}
   """
   def put(grid, cell), do: %{grid | lookup: Map.put(grid.lookup, {cell.row, cell.column}, cell)}
 
   @doc """
   Returns a cell from the grid.
-
-  ## Example:
-
-    iex> grid = Grid.new(2, 2)
-    iex> Grid.get(grid, {0, 0})
-    %Grid.Cell{walls: %{east: true, north: true, south: true, west: true}, row: 0, column: 0}
   """
   @spec get(t(), cell_position()) :: Cell.t()
   def get(grid, cell_position), do: Map.get(grid.lookup, cell_position)
+
+  @doc """
+  Returns a neighbor of current cell position in the given direction.
+  """
+  @spec get_neighbor(direction(), cell_position() | Cell.t(), t()) :: Cell.t() | nil
+  def get_neighbor(direction, %Cell{row: row, column: column}, grid),
+    do: get_neighbor(direction, {row, column}, grid)
+
+  def get_neighbor(direction, {row, column} = _current_cell, grid) do
+    case direction do
+      :east -> get(grid, {row, column + 1})
+      :south -> get(grid, {row + 1, column})
+      :west -> get(grid, {row, column - 1})
+      :north -> get(grid, {row - 1, column})
+    end
+  end
+
+  @doc """
+  Carves a passage between two cells in the grid by removing the walls between them.
+  The function accepts a direction, the current cell position, and the grid and returns the updated grid.
+  """
+  @spec carve_passage(direction(), cell_position(), t()) :: t()
+  def carve_passage(direction, current_cell_position, grid) do
+    current_cell = get(grid, current_cell_position)
+    updated_cell = Cell.remove_wall(current_cell, direction)
+
+    neighbor = get_neighbor(direction, current_cell_position, grid)
+    neighbor_opposite_direction = opposite_direction(direction)
+    updated_neighbor = Cell.remove_wall(neighbor, neighbor_opposite_direction)
+
+    grid
+    |> put(updated_cell)
+    |> put(updated_neighbor)
+  end
+
+  defp opposite_direction(:east), do: :west
+  defp opposite_direction(:south), do: :north
+  defp opposite_direction(:west), do: :east
+  defp opposite_direction(:north), do: :south
 end
